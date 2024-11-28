e72589b81c031255c5da773b700b357621b0278c
PoshiloySmih <mrforxbox@mail.ru>
2024-11-28 17:37:51 +0300

    Work in progress

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +26,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
-    TelegramBot bot = new TelegramBot("");
+    TelegramBot bot = new TelegramBot("${TELEGRAM_TOKEN}");
 
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
-            update.message().text();
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
