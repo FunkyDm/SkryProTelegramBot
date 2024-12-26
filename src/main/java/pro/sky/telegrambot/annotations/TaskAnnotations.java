9bc08f613edf25f4cca2cb9556fd882c0045b79a
FunkyDm <mrforxbox@mail.ru>
2024-12-26 17:35:28 +0300

    Work in progress

---
+++ src/main/java/pro/sky/telegrambot/annotations/TaskAnnotations.java
@@ -0,0 +1,10 @@
+package pro.sky.telegrambot.annotations;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "tasks")
+@Entity
+public class TaskAnnotations {
+
+}
--- src/main/resources/luquibase/scripts/user.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset funkyDM:1
-CREATE TABLE tasks (
-    ID SERIAL,
-    chat_ID long,
-    message String,
-
-)
