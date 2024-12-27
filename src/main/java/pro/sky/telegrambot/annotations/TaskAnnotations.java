cf248e3efd13995f7e132e53fbb905ee15c427c4
FunkyDm <mrforxbox@mail.ru>
2024-12-27 16:10:14 +0300

    Work in progress

--- src/main/java/pro/sky/telegrambot/annotations/TaskAnnotations.java
+++ src/main/java/pro/sky/telegrambot/annotations/TaskAnnotations.java
@@ -1,10 +1,18 @@
-package pro.sky.telegrambot.annotations;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Table(name = "tasks")
-@Entity
-public class TaskAnnotations {
-
-}
+//package pro.sky.telegrambot.annotations;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Table(name = "tasks")
+//@Entity
+//public class TaskAnnotations {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int taskId;
+//    @Column(name = "chat_id")
+//    private int chatId;
+//    @Column(name = "message_text")
+//    private String messageText;
+//    @Column(name = "chat_id")
+//    LocalDateTime dateTime;
+//}
---
+++ src/main/resources/luquibase/scripts/change-column-type-datetime.sql
@@ -0,0 +1,5 @@
+--luquibase formatted sql
+
+--changeset Dm:4
+
+ALTER TABLE tasks ALTER COLUMN date_time DATETIME;
