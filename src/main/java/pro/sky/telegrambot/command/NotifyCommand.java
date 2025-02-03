0d80656b8bf48a19002bfafd583cffa7232acec7
FunkyDm <mrforxbox@mail.ru>
2025-02-03 17:41:36 +0300

    in progress

--- src/main/java/pro/sky/telegrambot/command/NotifyCommand.java
+++ src/main/java/pro/sky/telegrambot/command/NotifyCommand.java
@@ -4,22 +4,35 @@ package pro.sky.telegrambot.command;
 import com.pengrad.telegrambot.model.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pro.sky.telegrambot.service.BotMessageService;
 import pro.sky.telegrambot.state.State;
 import pro.sky.telegrambot.state.UserStateStorage;
 
 @Component
-public class NotifyCommand implements Command{
+public class NotifyCommand implements Command {
+    private final BotMessageService botMessageService;
+
+    public NotifyCommand(BotMessageService botMessageService) {
+        this.botMessageService = botMessageService;
+    }
+
     @Override
-    public void handle(Update update){
+    public void handle(Update update) {
         Long chatId = update.message().chat().id();
         String messageText = update.message().text();
 
         State currentState = UserStateStorage.getState(chatId);
-        
+
+        if (messageText.equals(getCommand()) && currentState == State.START) {
+            botMessageService.sendMessage(chatId, "Отправьте напоминание в формате: ДД.ММ.ГГГГ ЧЧ:ММ Текст напоминания");
+            UserStateStorage.setState(chatId, State.IN_WORK_WITH_NOTIFICATION);
+        }
+
+
     }
 
     @Override
-    public String getCommand(){
+    public String getCommand() {
         return "/notify";
     }
 }
