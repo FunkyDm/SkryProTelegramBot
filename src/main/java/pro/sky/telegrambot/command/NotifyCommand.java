2890a501fa99a93e4deacedf2655000cfc229706
FunkyDm <mrforxbox@mail.ru>
2025-02-10 06:41:47 +0300

    in progress

--- src/main/java/pro/sky/telegrambot/command/NotifyCommand.java
+++ src/main/java/pro/sky/telegrambot/command/NotifyCommand.java
@@ -34,25 +34,27 @@ public class NotifyCommand implements Command {
         Long chatId = update.message().chat().id();
         String messageText = update.message().text();
         UserStateStorage currentState = UserStateStorage.getState(chatId);
-
         logger.info("Current state for chatId {}: {}", chatId, currentState);
 
         if (messageText.equals(getCommand()) && currentState == UserStateStorage.WORK) {
             logger.info("Command /notify received for chatId: {}", chatId);
 
             botMessageService.sendMessage(chatId, "Отправьте напоминание в формате: ДД.ММ.ГГГГ ЧЧ:ММ Текст напоминания");
-
-            logger.info("Sent message: Отправьте напоминание в формате: ДД.ММ.ГГГГ ЧЧ:ММ Текст напоминания")
+            logger.info("Sent message: Отправьте напоминание в формате: ДД.ММ.ГГГГ ЧЧ:ММ Текст напоминания");
 
             UserStateStorage.setState(chatId, UserStateStorage.IN_WORK_WITH_NOTIFICATION);
-
+            logger.info("User state set to IN_WORK_WITH_NOTIFICATION for chatId: {}", chatId);
         }
 
         if (currentState == UserStateStorage.IN_WORK_WITH_NOTIFICATION) {
+            logger.info("Entered WAITING_FOR_NOTIFICATION state for chatID: {}", chatId);
+
             Pattern pattern = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4}\\s\\d{2}:\\d{2})(\\s+)(.+)");
             Matcher matcher = pattern.matcher(messageText);
 
             if (matcher.matches()) {
+                logger.info("Pattern matched for message: {}", messageText);
+
                 String dateTimeString = matcher.group(1);
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
                 LocalDateTime date = LocalDateTime.parse(dateTimeString, formatter);
@@ -62,11 +64,18 @@ public class NotifyCommand implements Command {
                         matcher.group(3),
                         date
                 ));
+                logger.info("Notification task saved for chatID: {}, notification: {}, date: {}", chatId, matcher.group(3), date);
 
                 botMessageService.sendMessage(chatId, String.format("Напоминание добавлено: %s %s", dateTimeString, matcher.group(3)));
+                logger.info("Sent message: {}", String.format("Напоминание добавлено: %s, %s", date, matcher.group(3)));
+
                 UserStateStorage.setState(chatId, UserStateStorage.WORK);
-            } else {
+                logger.info("User state set to WORK for chatID: {}", chatId);
+            }
+            else {
+                logger.info("Pattern did not match for message: {}", messageText);
                 botMessageService.sendMessage(chatId, "Неверный формат напоминания. Используйте формат: ДД.ММ.ГГГГ ЧЧ:ММ Текст напоминания");
+                logger.info("Sent message: Неверный формат напоминания. Пожалуйста, используйте формат: ДД.ММ.ГГГГ ЧЧ:ММ Текст напоминания");
             }
         }
     }
