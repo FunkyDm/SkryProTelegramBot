df39b8f8801343d9ec3008596e7da4e9aa32c5bd
FunkyDm <mrforxbox@mail.ru>
2025-02-14 17:02:55 +0300

    Работа с тестами

--- src/main/java/pro/sky/telegrambot/service/BotMessageServiceImpl.java
+++ src/main/java/pro/sky/telegrambot/service/BotMessageServiceImpl.java
@@ -2,13 +2,15 @@ package pro.sky.telegrambot.service;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BotMessageServiceImpl implements BotMessageService{
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
+
+    public BotMessageServiceImpl(TelegramBot telegramBot){
+        this.telegramBot = telegramBot;
+    }
 
     @Override
     public void sendMessage(Long chatId, String message) {
--- src/test/java/pro/sky/telegrambot/service/BotMessageServiceTest.java
+++ src/test/java/pro/sky/telegrambot/service/BotMessageServiceTest.java
@@ -2,6 +2,7 @@ package pro.sky.telegrambot.service;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramException;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,15 +17,21 @@ public class BotMessageServiceTest {
     @BeforeEach
     public void init(){
         telegramBot = Mockito.mock(TelegramBot.class);
+        botMessageService = new BotMessageServiceImpl(telegramBot);
     }
 
     @Test
     public void shouldProperlySendMessage() throws TelegramException {
         //given
+        Long chatId = 1L;
+        String message = "testMessage";
+
+        SendMessage sendMessage = new SendMessage(chatId, message);
 
         //when
+        botMessageService.sendMessage(chatId, message);
 
         //then
-
+        Mockito.verify(telegramBot).execute(sendMessage);
     }
 }
